2b9297d557eb3ed395950ddda8d9af03b951517d
Kyle Mwendwa <kklegendofgaming@gmail.com>
2025-04-14 09:55:43 +0300

    table dummy content

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,9 +1,35 @@
 const Table = () => {
   return (
   <div>
-<table border="1">
+<table border="2">
 <tr>
-  th*
+  <th width="400px">Expense</th>
+  <th width="250px">Description</th>
+  <th width="200px">Category</th>
+  <th width="150px">Amount</th>
+  <th width="150px">Date</th>
+</tr>
+<tr>
+<td>Fried Chicken</td>
+<td>From KFC</td>
+<td>Food</td>
+<td>1600shs</td>
+<td>02/20/2025
+</td>
+</tr>
+<tr>
+  <td>Prostitutes</td>
+  <td>From Koinange</td>
+  <td>Leisure</td>
+  <td>150shs</td>
+  <td>04/13/2025</td>
+</tr>
+<tr>
+  <td>Bulletproof</td>
+  <td>From Naivas</td>
+  <td>Leisure</td>
+  <td>49shs</td>
+  <td>03/24/2025</td>
 </tr>
 </table>
   </div>
